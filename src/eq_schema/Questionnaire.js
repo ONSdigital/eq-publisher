91e2284706a68ad5697f8b0839d13180564decdc
WickyNilliams <wicky@nillia.ms>
2017-07-11 14:30:47 +0100

    use v1.0.2 of shared eslint config
    * fix indentation issues
    * remove package-lock.json
    * remove unnecessary dependency on eslint-config-jest (it's already a dependency of eslint-config-ons-author)

--- src/eq_schema/Questionnaire.js
+++ src/eq_schema/Questionnaire.js
@@ -1,22 +1,21 @@
-const Group = require('./Group');
+/* eslint-disable camelcase */
+const Group = require("./Group");
 
 class Questionnaire {
+  constructor(authorJson) {
+    this.mime_type = "application/json/ons/eq";
+    this.schema_version = "0.0.1";
+    this.data_version = "0.0.1";
+    this.survey_id = authorJson.id.toString();
+    this.title = authorJson.title;
+    this.groups = this.buildGroups(authorJson.groups);
+    this.theme = authorJson.theme;
+    this.legal_basis = authorJson.legalBasis;
+  }
 
-    constructor(authorJson) {
-        this.mime_type = "application/json/ons/eq";
-        this.schema_version = "0.0.1";
-        this.data_version = "0.0.1";
-        this.survey_id = authorJson.id.toString();
-        this.title = authorJson.title;
-        this.groups = this.buildGroups(authorJson.groups);
-        this.theme = authorJson.theme;
-        this.legal_basis = authorJson.legalBasis;
-    }
-
-    buildGroups(groups) {
-        return groups.map(group => new Group(group));
-    }
-
+  buildGroups(groups) {
+    return groups.map(group => new Group(group));
+  }
 }
 
 module.exports = Questionnaire;
