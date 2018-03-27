3f5a45e781244eccee61dbb4ce50941d5a074789
WickyNilliams <wicky@nillia.ms>
2018-03-27 13:28:23 +0100

    clean up ValidationApi tests

--- src/validation/ValidationApi.test.js
+++ src/validation/ValidationApi.test.js
@@ -12,13 +12,17 @@ describe("ValidationApi", () => {
     let mockRequest;
 
     beforeEach(() => {
-      mockRequest = jest.genMockFromModule("request-promise");
+      mockRequest = {
+        post: jest.fn()
+      };
+
       validationApi = new ValidationApi(url, mockRequest);
     });
 
     it("should pass the json to validation Api", () => {
       const json = { test: "json" };
       validationApi.validate(json);
+
       expect(mockRequest.post).toHaveBeenCalledWith(url, {
         body: json,
         json: true
@@ -26,7 +30,8 @@ describe("ValidationApi", () => {
     });
 
     it("should return valid response", () => {
-      mockRequest.post = jest.fn(() => Promise.resolve({}));
+      mockRequest.post.mockImplementation(() => Promise.resolve({}));
+
       expect(validationApi.validate({ test: "json" })).resolves.toEqual({
         valid: true
       });
@@ -39,7 +44,7 @@ describe("ValidationApi", () => {
       };
 
       it("should return invalid response", () => {
-        mockRequest.post = jest.fn(() => Promise.resolve({ errors }));
+        mockRequest.post.mockImplementation(() => Promise.resolve({ errors }));
 
         expect(
           validationApi.validate({ test: "json" })
@@ -50,7 +55,7 @@ describe("ValidationApi", () => {
       });
 
       it("should handle non-200 reponses", () => {
-        mockRequest.post = jest.fn(() =>
+        mockRequest.post.mockImplementation(() =>
           Promise.reject({
             response: {
               body: { errors }
