e5b40946d356dfeb567b9ce28719e67ba799fce4
Samiwel Thomas <samiwel.thomas@gmail.com>
2018-10-22 14:14:02 +0100

    Fix mutually exclusive checkbox logic.
    The pattern for mutually exclusive checkbox answers has recently been updated.
    
    Previously there used to be a specific answer type for mutually exclsive checkboxes. Now, the mutually exclusive pattern can theroetically apply to any answer type by marking the question as being a MutuallyExclusive question type and ensuring that the last answer on the page is the mutually exclusive checkbox option.
    
    This change changes existing logic for checkboxes. After applying this fix, published schema should provide respondents with a separate checkbox answer on the question page as a mutually exclusive option.

--- src/eq_schema/Question.js
+++ src/eq_schema/Question.js
@@ -1,11 +1,25 @@
 const Answer = require("./Answer");
 const { parseGuidance, getInnerHTMLWithPiping } = require("../utils/HTMLUtils");
-const { find, get, flow, flatten, isNil, assign } = require("lodash/fp");
+const {
+  find,
+  get,
+  flow,
+  flatten,
+  isNil,
+  assign,
+  concat,
+  omit
+} = require("lodash/fp");
 const convertPipes = require("../utils/convertPipes");
 
 const findDateRange = flow(
   get("answers"),
   find({ type: "DateRange" })
+);
+
+const findMutuallyExclusive = flow(
+  get("answers"),
+  find(answer => !isNil(get("mutuallyExclusiveOption", answer)))
 );
 
 const processPipedText = ctx =>
@@ -28,9 +42,15 @@ class Question {
 
     const dateRange = findDateRange(question);
 
+    const mutuallyExclusive = findMutuallyExclusive(question);
+
     if (dateRange) {
       this.type = "DateRange";
       this.answers = this.buildDateRangeAnswers(dateRange);
+    } else if (mutuallyExclusive) {
+      this.type = "MutuallyExclusive";
+      this.mandatory = get("properties.required", mutuallyExclusive);
+      this.answers = this.buildMutuallyExclusiveAnswers(mutuallyExclusive);
     } else {
       this.type = "General";
       this.answers = this.buildAnswers(question.answers);
@@ -58,6 +78,20 @@ class Question {
         new Answer(
           assign(childAnswer, { type: "Date", properties: answer.properties })
         )
+    );
+  }
+
+  buildMutuallyExclusiveAnswers(mutuallyExclusive) {
+    const mutuallyExclusiveAnswer = new Answer({
+      ...omit("other", mutuallyExclusive),
+      id: `${mutuallyExclusive.id}-exclusive`,
+      type: "Checkbox",
+      options: [mutuallyExclusive.mutuallyExclusiveOption]
+    });
+
+    return concat(
+      this.buildAnswers([mutuallyExclusive]),
+      mutuallyExclusiveAnswer
     );
   }
 }
